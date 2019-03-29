bc3a3415a02743e5af99931f0cd185734e12425a
EMAE05811 <ext.manuel.adele@sncf.fr>
2019-03-29 18:56:58 +0100

    Second commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+/**
+ * MDBootstrap PRO
+ */
+import { MDBBootstrapModulesPro } from 'ng-uikit-pro-standard';
+
+import { MDBSpinningPreloader } from 'ng-uikit-pro-standard';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MDBBootstrapModulesPro.forRoot(),
+  ],
+  providers: [
+    MDBSpinningPreloader,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
